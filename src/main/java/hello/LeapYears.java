7001554c79d4651d5ebbe33e2682eb3f732878c2
Paladin <74495854+Paladin-972@users.noreply.github.com>
2021-05-05 17:48:47 +0200

    revert

--- src/main/java/hello/LeapYears.java
+++ src/main/java/hello/LeapYears.java
@@ -6,6 +6,9 @@ public class LeapYears {
         if (year % 400 == 0) {
             return true;
         }
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
         return false;
     }
 }
