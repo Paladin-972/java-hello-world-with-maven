bf2e769280f49482584509bd97a18944a6d5560d
Paladin <74495854+Paladin-972@users.noreply.github.com>
2021-05-05 17:41:26 +0200

    cas erreur

--- src/main/java/hello/LeapYears.java
+++ src/main/java/hello/LeapYears.java
@@ -6,9 +6,6 @@ public class LeapYears {
         if (year % 400 == 0) {
             return true;
         }
-        if (year % 4 == 0 && year % 100 != 0) {
-            return true;
-        }
         return false;
     }
 }
